ca58d72e91607156048fb037ff6c99ff13d9cfc7
20R01A67H4 <117569824+20R01A67H4@users.noreply.github.com>
2023-10-20 18:24:21 +0530

    Add files via upload

---
+++ JS/EventHandling.js
@@ -0,0 +1,19 @@
+// document.getElementById('one').addEventListener('click',()=>{
+//     document.getElementById('hello').style.backgroundColor='red';
+// })
+// document.getElementById('one').addEventListener('mouseover',()=>{
+//     document.getElementById('one').removeEventListener('click',()=>{  //Not working
+//         document.getElementById('hello').style.backgroundColor='red';
+//     })
+//     document.getElementById('hello').style.backgroundColor='yellow';
+// })
+
+// let a=()=>{
+//     document.getElementById('hello').style.backgroundColor='red';
+// }
+// document.getElementById('one').addEventListener('click',a)
+// document.getElementById('one').addEventListener('mouseover',()=>{
+//     document.getElementById('one').removeEventListener('click',a) ///Just give reference of function to work
+//     document.getElementById('hello').style.backgroundColor='yellow';
+// })
+
---
+++ JS/EventLoop.js
@@ -0,0 +1,19 @@
+// setTimeout(function timer() {
+//     console.log('You clicked the button!');    //6
+// }, 1000);
+
+// console.log("Hi!");//1
+
+// let b=2;
+// console.log(b)//2
+
+// let c=3
+// console.log(c)//3
+
+// console.log("Hey");//4
+
+// setTimeout(function timeout() {
+// console.log("Click the button!");//7
+// }, 5000);
+
+// console.log("Welcome to loupe.");//5
